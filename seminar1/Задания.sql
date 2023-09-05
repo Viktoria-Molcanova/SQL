37eefcc41f44128e9db3bdfdb07fbcf66c35ac06
Viktoria-Molcanova <119111084+Viktoria-Molcanova@users.noreply.github.com>
2023-09-05 22:50:48 +0700

    Add files via upload

---
+++ seminar1/Задания.sql
@@ -0,0 +1,33 @@
+//2 SELECT-запрос, который выводит название товара, производителя и цену для товаров, количество //которых превышает 2
+
+SELECT manufacturer, product_name, price
+FROM sakila.1
+WHERE product_count > 2;
+
+//3 Выведите SELECT-запросом весь ассортимент товаров марки “Samsung”
+SELECT *
+FROM sakila.1
+WHERE manufacturer = 'Samsung';
+
+//4 Товары, в которых есть упоминание "Iphone"
+SELECT *
+FROM sakila.1
+WHERE product_name LIKE '%Iphone%';
+
+//5 Товары, в которых есть упоминание "Samsung"
+
+SELECT *
+FROM sakila.1
+WHERE manufacturer LIKE '%Samsung%';
+
+//5  Товары, в названии которых есть ЦИФРЫ
+
+SELECT *
+FROM sakila.1
+WHERE product_name LIKE '%[0-9]%';
+
+//6  Товары, в названии которых есть ЦИФРА "8"
+
+SELECT *
+FROM sakila.1
+WHERE product_name LIKE '%8%';
