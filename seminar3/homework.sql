38f03ccfeffee7067773f92147c4784d017b4ef6
Viktoria-Molcanova <119111084+Viktoria-Molcanova@users.noreply.github.com>
2023-09-12 22:22:34 +0700

    Update and rename 1 to homework.sql

---
+++ seminar3/homework.sql
@@ -0,0 +1,21 @@
+use vk
+  -- Написать скрипт, возвращающий список имен (только firstname) пользователей без повторений в алфавитном порядке. [ORDER BY]
+  
+SELECT DISTINCT firstname
+FROM `vk`.`users`
+ORDER BY firstname ASC;
+
+ -- Выведите количество мужчин старше 35 лет [COUNT].
+
+SELECT COUNT(*) 'Количество мужчин старше 35 лет'
+FROM `vk`.`profiles` 
+WHERE gender = 'm' 
+    AND TIMESTAMPDIFF(YEAR, birthday, NOW()) > '35';
+
+-- Сколько заявок в друзья в каждом статусе? (таблица friend_requests) [GROUP BY]
+
+SELECT status, COUNT(*) 'Количество заявок в друзья'
+FROM `vk`.`friend_requests` 
+GROUP BY status;
+
+
